d62ceb4b1b026777eab5aedd258ba87ae7c78ed1
Aleksey <limon260192@gmail.com>
2023-06-15 20:57:38 +0900

    Решил все)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.Write("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+//     System.Console.WriteLine($"Большее число: { a }, меньшее число: { b }");
+// else System.Console.WriteLine($"Большее число: { b }, меньшее число: { a }");
+
+
+
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+// System.Console.WriteLine($"Максимальное из этих чисел {max}");
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Write("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+//     System.Console.WriteLine(" да ");
+// else System.Console.WriteLine(" нет ");
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= n ; i++)
+// {
+//     if ((i % 2 == 0) && ( i != n) && (i != n-1))
+//         Console.Write($"{i}, ");
+//     if ((i == n -1) && (i % 2 == 0))
+//         {Console.Write($"{i}");}
+//     if ((i == n) && (i % 2 == 0))
+//         {Console.Write($"{i}");}
+// }
+
